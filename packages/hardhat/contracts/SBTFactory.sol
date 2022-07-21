94e3effa3a246163de8e26332bec96bf517c29ca
Iuri <iuri.19.05@gmail.com>
2022-07-21 17:01:16 +0200

    feat: Test the functions to store a Test in ipfs and create a SBT

--- packages/hardhat/contracts/SBTFactory.sol
+++ packages/hardhat/contracts/SBTFactory.sol
@@ -34,7 +34,7 @@ contract SBTFactory {
     function createSBT(string calldata _name, string calldata _symbol) external onlyAllowedAccounts returns (address) {
         address clone = Clones.clone(tokenImplementation);
         SBT(clone).initialize(_name, _symbol);
-        sbt.push(clone);
+        sbt.push(SBT(clone));
         emit CreateSBT(msg.sender,clone);
         return clone;
     }
--- packages/react-app/src/services/interaction_SBTFactory.js
+++ packages/react-app/src/services/interaction_SBTFactory.js
@@ -1,7 +1,7 @@
 import { ethers } from 'ethers';
-import SBTFactory from '../../../hardhat/artifacts/contracts/SBTFactory.sol/SBTFactory.json';
+import SBTFactory from '../contracts/SBTFactory.sol/SBTFactory.json';
 
-const SBTFactoryAddress = '';
+const SBTFactoryAddress = '0x9fe46736679d2d9a65f0992f2272de9f3c7fa6e0';
 
 async function requestAccount() {
     await window.ethereum.request({ method: 'eth_requestAccounts' });
