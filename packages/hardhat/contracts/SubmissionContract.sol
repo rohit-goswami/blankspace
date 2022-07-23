dbd28375c0214d0cc875b64fb5c1f00ab845c638
Josep Chetrit <jchetrit1999@gmail.com>
2022-07-22 22:15:00 -0400

    Adding the new features in the smart contracts #55

--- packages/hardhat/contracts/SubmissionContract.sol
+++ packages/hardhat/contracts/SubmissionContract.sol
@@ -13,12 +13,13 @@ contract SubmissionContract {
         Result result;
     }
 
-    Submission[] listOfAllSubmisions;
-    mapping(address => Submission[]) listOfAllSubmissionsOfATest;
+    mapping(address => string[]) public listOfAllSubmissionsOfATest; // Test -> Array uid Submissions
+    mapping(string => Submission) public listOfSubmissions; // uid Submissions -> Submission
 
     address owner;   
 
-    event NewSubmission(address indexed from, address indexed test, uint256 indexed date);
+    event NewSubmission(address indexed from, address indexed test, string indexed uid);
+    event CorrectedSubmission(address indexed reviser, string indexed uid);
 
     modifier onlyOwner() {
         require(msg.sender == owner, "You're not the owner of this smart contract");
@@ -29,7 +30,7 @@ contract SubmissionContract {
         owner = msg.sender;
     }
 
-    function newSubmission(string calldata _cidSubmission, address _test) public onlyOwner {
+    function newSubmission(string calldata _cidSubmission, address _test, string calldata _uid) public onlyOwner {
         Submission memory submission = Submission(
             _cidSubmission,
             tx.origin,
@@ -37,16 +38,29 @@ contract SubmissionContract {
             block.timestamp,
             Result.PENDING
         );
-        listOfAllSubmisions.push(submission);
-        listOfAllSubmissionsOfATest[_test].push(submission);
-        emit NewSubmission(tx.origin,_test,block.timestamp);
+        listOfAllSubmissionsOfATest[_test].push(_uid);
+        listOfSubmissions[_uid] = submission;
+        emit NewSubmission(tx.origin,_test,_uid);
     }
 
-    function getAllSubmissions() public view returns(Submission[] memory){
-        return listOfAllSubmisions;
+    function setResultSubmission(string calldata _uid, Result _result) public onlyOwner {
+        require(listOfSubmissions[_uid].result == Result.PENDING,"This submission has already been corrected");
+        require(_result == Result.PASSED || _result == Result.FAILED);
+        listOfSubmissions[_uid].result = _result;
+        emit CorrectedSubmission(tx.origin,_uid);
+
     }
 
-    function getAllSubmissionsOfATest(address _test) public view returns(Submission[] memory){
+    function getResultSubmission(string calldata _uid) public view returns (Result){
+        return listOfSubmissions[_uid].result;
+    }
+
+    function getAllSubmissionsOfATest(address _test) public view returns(string[] memory){
         return listOfAllSubmissionsOfATest[_test];
     }
+
+    function getSubmission(string calldata _uid) public view returns (Submission memory) {
+        return listOfSubmissions[_uid];
+    }
+
 }
